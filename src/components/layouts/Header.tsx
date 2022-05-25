c31187ad67304cffe6eb7815b23d2d7b6728ea4d
rain-bell17 <junya_1214@icloud.com>
2022-05-26 01:26:39 +0900

    remove:Header&Footer

--- src/components/layouts/Header.tsx
+++
@@ -1,6 +0,0 @@
-import { styled } from 'linaria/react'
-import React from 'react'
-
-export const Header: React.FC = () => {
-  return <></>
-}
--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,8 +1,6 @@
 import { styled } from 'linaria/react'
 import React from 'react'
 import { reset } from '../../styles/global'
-import { Footer } from './Footer'
-import { Header } from './Header'
 
 type Props = {
   children?: React.ReactNode
@@ -12,9 +10,7 @@ export const Layout: React.FC<Props> = (props) => {
   return (
     <>
       <LWrap className={reset}>
-        <Header />
         {props.children}
-        <Footer />
       </LWrap>
     </>
   )
